237819afa32b31a58057b350920fbd3e819a3733
Rahul Kumar <rkumar91058@gmail.com>
2025-06-30 10:55:32 +0530

    day3

---
+++ Day 3/MaximumSubArray.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        System.out.println("The maximum subarray sum is: " + maxSum);
+        sc.close();
+    }
+    
+}
